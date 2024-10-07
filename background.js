d0ad9feefca4c1925d044d07c3861656ed01e0ca
Super_String <superstringtheory@foxmail.com>
2024-10-07 19:44:47 +0800

    代码结构优化

--- background.js
+++ background.js
@@ -1,53 +1,31 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("My Chrome Extension installed!");
+    console.log("Chrome Extension installed！");
 });
 
 // background.js
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.type === 'STORE_VIDEO_INFO') {
-        // 处理接收到的信息
-        const bv = message.bv;
-        const title = message.title;
-        const currentTime = message.currentTime;
-
-        console.log('Received BV ID:', bv);
-        console.log('Received Current Time:', currentTime);
-
-        // 在这里可以把信息存储起来，例如存储在 chrome.storage 中
-        chrome.storage.local.set({[bv] : {currentTime: currentTime, title: title}}, function() {
-            console.log('Video information saved.');
+        const {bv, title, currentTime} = message;
+        chrome.storage.local.set({[bv] : {currentTime, title}}, () => {
+            console.log('Video information saved:', {bv, title, currentTime});
         });
 
-        chrome.storage.local.get(null, function(items) {
-            console.log('All stored items:', items);
-            // 在这里可以对 items 对象进行各种处理
-            for (let key in items) {
-                console.log(`Key: ${key}，title:${items[key]['title']}，currentTime: ${items[key]['currentTime']}`);
-            }
-        });
-        // 发送回应给 content.js
-        console.log("wwwwwww!!!")
-        // // 设置 badge 文本
+        // 设置 badge
         chrome.browserAction.setBadgeText({ text: '更新!' });
-        // // 设置 badge 背景颜色
-        chrome.browserAction.setBadgeBackgroundColor({ color: 'green' }); // 红色背景
-        // 3 秒后清除 badge
+        chrome.browserAction.setBadgeBackgroundColor({ color: 'green' });
         setTimeout(() => {
-            chrome.browserAction.setBadgeText({ text: '' }); // 清空 badge
+            chrome.browserAction.setBadgeText({ text: '' });
         }, 3000);
 
         sendResponse({status: 'success'});
 
-    } else if(message.type =='SKIP_TO_VIDEO'){
-        const url = message.url;
-        const currentTime = message.currentTime;
-        console.log("Receive VIDEO_SKIP Message!")
-        chrome.tabs.create({ url: url },function(tab){
+    } else if(message.type =='SKIP_TO_VIDEO'){ //跳转
+        const {url, currentTime} = message.url;
+        // 页面加载完成后触发跳转
+        chrome.tabs.create({ url },function(tab){
             function handleTabUpdate(tabId, changeInfo, updatedTab) {
                 if (tabId === tab.id && changeInfo.status === 'complete') {
-                    // 页面加载完成后发送消息
-                    chrome.tabs.sendMessage(tab.id, { currentTime: currentTime });
-                    console.log("已经发送！");
+                    chrome.tabs.sendMessage(tab.id, { currentTime });
                     // 移除监听器，避免重复添加
                     chrome.tabs.onUpdated.removeListener(handleTabUpdate);
                 }
